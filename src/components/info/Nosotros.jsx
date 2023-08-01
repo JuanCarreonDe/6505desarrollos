f591e5a3c733e7042596aa91712a7e78bf25a41c
Juan Carreon <juancarreondelira@gmail.com>
2023-08-01 09:57:16 -0600

    segundo commit

--- src/components/info/Nosotros.jsx
+++ src/components/info/Nosotros.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Nosotros = () => {
   return (
     <>
-      <h1 className="info__h1">Nosotros</h1>
+    <div className="info__title">
+      <span className="info__span"></span>
+    <h1 className="info__h1">Nosotros</h1>
+    </div>
       <p className="info__p">Nos apasiona impulsar la innovación y transformar ideas en soluciones tecnológicas de vanguardia. Estamos comprometidos con ofrecer productos y servicios de calidad que ayuden a nuestros clientes a alcanzar sus objetivos y superar los desafíos del mundo digital.</p>
       <ul className="info__ul">
         <li className="info__li">Soluciones novedosas y creativas</li>
--- src/components/info/Servicios.jsx
+++ src/components/info/Servicios.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Servicios = () => {
   return (
     <>
+        <div className="info__title">
+      <span className="info__span"></span>
       <h1 className="info__h1">Servicios</h1>
+      </div>
       <p className='info__p'>Estamos emocionados de presentarles nuestros servicios innovadores y vanguardistas que impulsarán el éxito de su empresa en la era digital.</p>
       <ul className="info__ul">
         <li className="info__li">Diseño de app</li>
